f4a6536766bf5c15101844169bbcbe4a7c251a82
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2017-05-22 16:40:30 -0400

    Added missing copyright

--- CRMAssemblyLoader/Properties/AssemblyInfo.cs
+++ CRMAssemblyLoader/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CRMAssemblyLoader")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2017 Wycliffe Associates")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
